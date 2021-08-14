4a87e1a0edb9108ca19017e1c1f6a336186de00c
CeliaLopes <oliveira.lopes.celia@gmail.com>
2021-08-14 17:52:08 -0300

    testecelia

---
+++ aula01.js
@@ -0,0 +1,61 @@
+//var nome = "Gab";
+//console.log(nome  + " é maravilhosa !!!!");
+//var peso = 55;
+//var altura = 1.65
+//var IMC = 0
+
+//IMC = (peso)/(altura^2)
+//console.log("O IMC = " + IMC);
+
+//var first = 10;
+//first++;
+//var value = 5 + ++first;
+//console.log(value)
+//console.log(first)
+
+//var first = 10;
+//var second = 3;
+
+//var result = first > second ? 'First é maior ' : 'Second é maior ';
+
+//console.log(result);
+
+//var nota1 = 8;
+//var nota2 = 7;
+
+//media = (nota1 + nota2)/2;
+//var result = media > 7 ? 'Aprovado' : 'Reprovado';
+//console.log(result)
+
+//var first = 'a';
+//var second = 'b';
+//var result = first == second ? 'igual ' : 'diferente';
+//console.log(result);
+
+//var bola, pipa;
+//bola = true;
+//pipa = false;
+
+//var resultado = bola && pipa;
+//console.log(resultado);
+
+var idade = 16;
+
+var resultado = idade >=16 && idade <= 18;
+resultado = 'meia entrada';
+console.log(resultado);
+
+var resultado = idade >=19 && idade <= 60;
+resultado = 'inteira';
+console.log(resultado);
+
+var resultado = idade >=60;
+resultado = 'meia entrada';
+console.log(resultado);
+
+
+
+
+
+
+
