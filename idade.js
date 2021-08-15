8ae60da06269ca5e44aa311d80b815bf7acf3598
CeliaLopes <oliveira.lopes.celia@gmail.com>
2021-08-14 22:06:01 -0300

    teste2

--- idade.js
+++ idade.js
@@ -2,4 +2,4 @@ var idade = 17;
 
 var verificar = (idade >=16 && idade <=18) || (idade >60) ? 'meia entrada' : 'entrada inteira';
 console.log('voce vai pagar: ' + verificar)
-git 22
+//Teste git hub push
